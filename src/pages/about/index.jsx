ce7f72a1de74a4b85efba8243a1bfcb013631926
MikaSRahwono <mikasuryof@outlook.com>
2023-07-18 02:27:20 +0700

    [FEAT] adding about page

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./style.css";
+
+export const About = () => {
+  return (
+    <div className="ABOUT">
+      <div className="div-css">
+        <div className="frame-3">
+          <div className="heading-kenalan">
+            <span className="text-wrapper-3">
+              Introducing
+              <br />
+            </span>
+            <span className="text-wrapper-4">DIGITAL FORGE</span>
+          </div>
+          <div className="div-html-content">
+            <div className="p">
+              <p className="flip-adalah">
+                Flip adalah perusahaan teknologi keuangan di Indonesia yang
+                <br />
+                menghadirkan solusi keuangan berbasis transfer uang. Sampai hari ini, Flip
+                <br />
+                sudah melayani lebih dari 12 juta pengguna dan 800 perusahaan serta
+                <br />
+                UKM.
+              </p>
+            </div>
+            <div className="namun-flip-memiliki-wrapper">
+              <p className="namun-flip-memiliki">
+                Namun, Flip memiliki tujuan lebih besar. Flip ingin membantu lebih banyak
+                <br />
+                masyarakat dan bisnis-bisnis untuk menumbuhkan dan mencapai financial
+                <br />
+                goal yang dicita-citakan. Karena itu, berbagai inovasi produk dan layanan
+                <br />
+                keuangan yang lebih fair terus kami ciptakan.
+              </p>
+            </div>
+            <div className="sekarang-pengguna-wrapper">
+              <p className="sekarang-pengguna">
+                Sekarang, pengguna bisa transfer beda bank dengan gratis, top up e-wallet
+                <br />
+                tanpa potongan atau tambahan biaya, dan kirim uang ke luar negeri dengan
+                <br />
+                jauh lebih hemat. Oh, juga, pengguna Flip bisa bayar tagihan listrik, beli
+                <br />
+                token listrik, isi pulsa, dan beli paket data dengan biaya paling murah.
+              </p>
+            </div>
+            <div className="untuk-perusahaan-wrapper">
+              <p className="untuk-perusahaan">
+                Untuk perusahaan atau pelaku bisnis, Flip menyediakan pula solusi
+                <br />
+                manajemen keuangan bisnis terbaik.
+              </p>
+            </div>
+          </div>
+        </div>
+        <img
+          className="asian-woman-showing"
+          alt="Asian woman showing"
+          src="https://generation-sessions.s3.amazonaws.com/ef0ebb8c6c1396e052f5ec2841181550/img/asian-woman-showing-phone-043ec50b1f-png.png"
+        />
+      </div>
+      <div className="div-css-vonkdt">
+        <div className="overlap-group">
+          <div className="div-css-fiub" />
+          <div className="div-chakra-stack">
+            <h1 className="heading-visi">Visi</h1>
+            <div className="heading">
+              <p className="menjadi-perusahaan">
+                Menjadi perusahaan dengan layanan paling customer-centric di
+                <br />
+                dunia dan memungkinkan pengguna untuk melakukan transaksi
+                <br />
+                keuangan yang adil dari mana saja kepada siapa pun.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
