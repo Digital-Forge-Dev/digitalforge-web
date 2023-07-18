2a1fd230d85b57632877e701cf7ebc8f91b93e7d
MikaSRahwono <mikasuryof@outlook.com>
2023-07-18 13:56:20 +0700

    [FIX] fixing footer section

---
+++ src/sections/common/footer/Logo.jsx
@@ -0,0 +1,19 @@
+import { Image } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+
+const LogoBox = styled.span`
+  display: inline-flex;
+  align-items: center;
+  height: 30px;
+  line-height: 20px;
+  padding: 5px;
+  margin-top: 10px;
+`;
+
+export function Logo() {
+  return (
+      <LogoBox>
+        <Image src={process.env.PUBLIC_URL + '/logo.png'} width={200} height={10} alt="logo" />
+      </LogoBox>
+  );
+}
